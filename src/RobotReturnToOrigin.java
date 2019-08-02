97661989508e46db0bfd328c9c686c703ec7531b
Zhicheng Wang <zhichengwangS@outlook.com>
2019-08-02 14:46:38 +1000

    657. Robot Return to Origin Faster than 94%

---
+++ src/RobotReturnToOrigin.java
@@ -0,0 +1,20 @@
+public class RobotReturnToOrigin {
+    public boolean judgeCircle(String moves) {
+        int a = 0;
+        int b = 0;
+        for (char m : moves.toCharArray()) {
+            if (m == 'U')
+                a++;
+            else if (m == 'D')
+                a--;
+            else if (m == 'R')
+                b++;
+            else
+                b--;
+        }
+        if (a == 0 && b == 0)
+            return true;
+        else
+            return false;
+    }
+}
