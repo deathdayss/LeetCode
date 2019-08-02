297da2704812d7869c6a3cf109fab7ed554ac5bb
Zhicheng Wang <zhichengwangS@outlook.com>
2019-08-02 14:56:49 +1000

    Doing PartitionLabels

---
+++ src/PartitionLabels.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels {
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> a = new ArrayList<Integer>();
+        int count = 0;
+        for (int i; i < S.length(); i++)) {
+            if ()
+        }
+    }
+}
