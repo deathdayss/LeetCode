7751baa3bf004514fecb2b2ff640dbf51ef19e3f
Zhicheng Wang <zhichengwangS@outlook.com>
2019-08-02 20:05:33 +1000

    Finished 763. PartitionLabels Faster than 10%

--- src/PartitionLabels.java
+++ src/PartitionLabels.java
@@ -6,16 +6,14 @@ import java.util.List;
 public class PartitionLabels {
     public static List<Integer> partitionLabels(String S) {
         List<Integer> a = new ArrayList<Integer>();
-        ArrayList<Character> b = new ArrayList<Character>();
         int count = 1;
         int g = 0;
         for (int i = 0; i < S.length(); i++) {
-            if (a.size() != 0)
-                g = a.get(a.size() - 1);
-            if (b.contains(S.charAt(i))) {
-                continue;
-            }
             if (i - g < count) {
+                if (i == S.length() - 1) {
+                    a.add(count);
+                    break;
+                }
                 for (int m = S.length() - 1; m >= 0; m--) {
                     if (m + 1 - g <= count) {
                         break;
@@ -25,22 +23,18 @@ public class PartitionLabels {
                         break;
                     }
                 }
-                b.add(S.charAt(i));
             }
             else {
                 a.add(count);
                 count = 1;
-                b.clear();
-                if (i < S.length() - 1)
-                    i--;
-                else
-                    a.add(count);
+                g = i;
+                i--;
             }
         }
         return a;
     }
 
     public static void main(String[] args) {
-        System.out.println(partitionLabels("abce").get(0));
+        System.out.println(partitionLabels("aabbc").size());
     }
 }
