50827d33e58f67b35e1f38a84011efd3668da371
Zhicheng Wang <zhichengwangS@outlook.com>
2019-09-06 10:52:52 +1000

    doing 954. Array of Doubled Pairs Time Limit Exceeded

---
+++ src/ArrayofDoubledPairs.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ArrayofDoubledPairs {
+    public boolean canReorderDoubled(int[] A) {
+        ArrayList<Integer> pInteger = new ArrayList<>();
+        ArrayList<Integer> ninteger = new ArrayList<>();
+        for (int i : A) {
+            if (i >= 0) {
+                if (pInteger.size() == 0) {
+                    pInteger.add(i);
+                } else if (pInteger.size() == 1){
+                    if (i < pInteger.get(0)) {
+                        pInteger.add(0, i);
+                    } else {
+                        pInteger.add(i);
+                    }
+                } else {
+                    if (i <= pInteger.get(0)) {
+                        pInteger.add(0,i);
+                    } else if (i >= pInteger.get(pInteger.size() - 1)) {
+                        pInteger.add(i);
+                    } else {
+                        for (int m = 0; m < pInteger.size(); m++) {
+                            if (i >= pInteger.get(m) && i <= pInteger.get(m + 1)) {
+                                pInteger.add(m + 1, i);
+                                break;
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (ninteger.size() == 0) {
+                    ninteger.add(i);
+                } else if (ninteger.size() == 1){
+                    if (i > ninteger.get(0)) {
+                        ninteger.add(0, i);
+                    } else {
+                        ninteger.add(i);
+                    }
+                } else {
+                    if (i >= ninteger.get(0)) {
+                        ninteger.add(0,i);
+                    } else if (i <= ninteger.get(ninteger.size() - 1)) {
+                        ninteger.add(i);
+                    } else {
+                        for (int m = 0; m < ninteger.size(); m++) {
+                            if (i <= ninteger.get(m) && i >= ninteger.get(m + 1)) {
+                                ninteger.add(m + 1, i);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(ninteger);
+        ninteger.addAll(pInteger);
+        System.out.println(ninteger);
+        for (int i = 0; i < ninteger.size(); i++) {
+            boolean isValid = false;
+            int twice = 0;
+            for (int w = i + 1; w < ninteger.size(); w++) {
+                 if (ninteger.get(w) == 2 * ninteger.get(i)) {
+                     isValid = true;
+                     twice = w;
+                     break;
+                 }
+            }
+            if (isValid) {
+                ninteger.remove(twice);
+                ninteger.remove(i);
+                i--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ArrayofDoubledPairs ad = new ArrayofDoubledPairs();
+        System.out.println(ad.canReorderDoubled(new int[]{-2,-6,-3,4,-4,2}));
+    }
+}
