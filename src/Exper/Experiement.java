f5e4fb8f51b2ab90e74fca598fb989627b8004c4
Zhicheng Wang <zhichengwangS@outlook.com>
2019-09-17 23:11:21 +1000

    sort a list of string

--- src/Exper/Experiement.java
+++ src/Exper/Experiement.java
@@ -40,10 +40,38 @@ class Experiement extends it {
         j[0] = 1;
         return j;
     }
+    public static String sortIt(String placement) {
+        String m = "";
+        ArrayList<Integer> myIntegers = new ArrayList<>();
+        ArrayList<Character> myChars = new ArrayList<>();
+        for (int i = 0; i < placement.length(); i += 4) {
+            if (myChars.size() == 0) {
+                myChars.add(placement.charAt(i));
+                myIntegers.add(i);
+            } else {
+                if (placement.charAt(i) <= myChars.get(0)) {
+                    myChars.add(0, placement.charAt(i));
+                    myIntegers.add(0, i);
+                } else if (placement.charAt(i) >= myChars.get(myChars.size() - 1)) {
+                    myChars.add(placement.charAt(i));
+                    myIntegers.add(i);
+                } else {
+                    for (int index = 0; index < myChars.size(); index++) {
+                        if(placement.charAt(i) > myChars.get(index) && placement.charAt(i) < myChars.get(index + 1)) {
+                            myChars.add(index + 1, placement.charAt(i));
+                            myIntegers.add(index + 1, i);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        for(Integer real : myIntegers) {
+            m += placement.substring(real, real + 4);
+        }
+        return m;
+    }
     public static void main(String[] args) {
-        int[] fs = new int[1];
-        int[] ks = add(fs);
-        ks[0] = 3;
-        System.out.println(fs[0]);
+        System.out.println(sortIt("h000a021d232i532e621g220j701b410c100f400"));
     }
 }
