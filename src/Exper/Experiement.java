33f8ed9dbe1de15768d155bb28447fa6efb47511
Zhicheng Wang <zhichengwangS@outlook.com>
2019-11-07 10:39:01 +1100

    Jump_Game (Time Limit Exceeded)

--- src/Exper/Experiement.java
+++ src/Exper/Experiement.java
@@ -1,5 +1,9 @@
 package Exper;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 class Experiement{
 
     public int[][][] findIt(int[][] tuple, int start) {
@@ -11,5 +15,37 @@ class Experiement{
     public int[][][] findCircle(int[][] tuple) {
 
         return findIt(tuple, 0);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+        a.add(7);
+        a.add(4);
+        a.add(1);
+        a.add(9);
+        a.add(1);
+        a.add(1);
+        System.out.println(x(a));
+    }
+    static List<Integer> x(List<Integer> in) {
+        if (in == null || in.size() <= 1)
+            return in;
+
+        int size = in.size();
+        List<Integer> l = x(in.subList(0, size/2));
+        List<Integer> r = x(in.subList(size/2, size));
+
+        int lf = 0;
+        int rf = 0;
+        List<Integer> result = new ArrayList<>(size);
+
+        while (lf < l.size() || rf < r.size()) {
+            if (lf == l.size() || (rf < r.size() && r.get(rf) < l.get(lf))) {
+                result.add(r.get(rf++));
+            } else {
+                result.add(l.get(lf++));
+            }
+        }
+        return result;
     }
 }
---
+++ src/Medium/Jump_Game.java
@@ -0,0 +1,33 @@
+package Medium;
+
+public class Jump_Game {
+
+    boolean canJump(int index, int[] nums) {
+        System.out.println("index is " + index);
+        for (int i = 0; i < index; i++) {
+            for (int j = 1; j <= nums[i]; j++) {
+                if (i + j == index) {
+                    if (i == 0) {
+                        return true;
+                    } else if (canJump(i, nums)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean canJump(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+        return canJump(nums.length - 1, nums);
+    }
+
+    public static void main(String[] args) {
+        Jump_Game a = new Jump_Game();
+        System.out.println(a.canJump(new int[] {3,2,1,0,4}));
+    }
+}
