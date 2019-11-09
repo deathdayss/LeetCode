68feb8340603615aa0142f1dd06a714048902144
Zhicheng Wang <zhichengwangS@outlook.com>
2019-11-09 19:21:58 +1100

    Modified experiment

--- src/Exper/Experiement.java
+++ src/Exper/Experiement.java
@@ -18,34 +18,6 @@ class Experiement{
     }
 
     public static void main(String[] args) {
-        List<Integer> a = new ArrayList<>();
-        a.add(7);
-        a.add(4);
-        a.add(1);
-        a.add(9);
-        a.add(1);
-        a.add(1);
-        System.out.println(x(a));
-    }
-    static List<Integer> x(List<Integer> in) {
-        if (in == null || in.size() <= 1)
-            return in;
-
-        int size = in.size();
-        List<Integer> l = x(in.subList(0, size/2));
-        List<Integer> r = x(in.subList(size/2, size));
-
-        int lf = 0;
-        int rf = 0;
-        List<Integer> result = new ArrayList<>(size);
-
-        while (lf < l.size() || rf < r.size()) {
-            if (lf == l.size() || (rf < r.size() && r.get(rf) < l.get(lf))) {
-                result.add(r.get(rf++));
-            } else {
-                result.add(l.get(lf++));
-            }
-        }
-        return result;
+        System.out.println(111&11);
     }
 }
