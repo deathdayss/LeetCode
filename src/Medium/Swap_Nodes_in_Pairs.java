1de55ed7b66b8f3aee8d7ba570c03fba175973fe
deathdayss <zhichengwangss@gmail.com>
2019-11-25 22:09:54 +0800

    Modified Swap_Nodes_in_Pairs

--- src/Medium/Swap_Nodes_in_Pairs.java
+++ src/Medium/Swap_Nodes_in_Pairs.java
@@ -2,9 +2,6 @@ package Medium;
 
 public class Swap_Nodes_in_Pairs {
     public ListNode swapPairs(ListNode head) {
-        if(head == null) {
-            return head;
-        }
         ListNode first = head;
         while(head != null) {
             int mid = head.val;
