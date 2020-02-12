5876dad53dfc2771a04720f5db3626a38c58b9cf
Zhicheng Wang <zhichengwangS@outlook.com>
2020-02-12 15:51:46 +1100

    Faster solution for BestTimetoBuyandSellStock
    Runtime: 1 ms, faster than 80.13% of Java online submissions for Best Time to Buy and Sell Stock.
    Memory Usage: 42.6 MB, less than 5.31% of Java online submissions for Best Time to Buy and Sell Stock.

--- src/dynamicprogramming/easy/BestTimetoBuyandSellStock.java
+++ src/dynamicprogramming/easy/BestTimetoBuyandSellStock.java
@@ -1,12 +1,22 @@
 package dynamicprogramming.easy;
 
 public class BestTimetoBuyandSellStock {
+    // Time: O(n^2) Space: O(n)
+//    public int maxProfit(int[] prices) {
+//        int mp = 0;
+//        for (int i = 0; i < prices.length; i++) {
+//            for (int j = i + 1; j < prices.length; j++) {
+//                mp = Math.max(mp, prices[j] - prices[i]);
+//            }
+//        }
+//        return mp;
+//    }
     public int maxProfit(int[] prices) {
         int mp = 0;
+        int mv = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                mp = Math.max(mp, prices[j] - prices[i]);
-            }
+            mp = Math.max(prices[i] - mv, mp);
+            mv = Math.min(prices[i], mv);
         }
         return mp;
     }
