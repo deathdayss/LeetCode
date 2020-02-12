7a2eb00730de5a5625ce24bd68c66643dbe63bfd
Zhicheng Wang <zhichengwangS@outlook.com>
2020-02-12 14:42:47 +1100

    Unfinished

---
+++ src/dynamicprogramming/medium/UniqueBinarySearchTreesII.java
@@ -0,0 +1,21 @@
+package dynamicprogramming.medium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class UniqueBinarySearchTreesII {
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode>[] dp = new LinkedList[n];
+        dp[0] = null;
+        for (int i = 1; i <= n; i++) {
+            dp[i] =
+        }
+    }
+}
