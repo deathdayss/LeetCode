ea8f5dfc015d97a9644356bab0637e99c1bda9bd
Zhicheng Wang <zhichengwangS@outlook.com>
2020-02-24 23:27:28 +1100

    Test git

--- src/Easy/RemoveOutermostParentheses.java
+++ src/Easy/RemoveOutermostParentheses.java
@@ -24,7 +24,7 @@ public class RemoveOutermostParentheses {
                 }
             }
         }
-        return  tar;
+        return tar;
     }
 
     public static void main(String[] args) {
